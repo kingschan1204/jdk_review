501aa9e01ca8da9d134aa1e83556de8ae29446ea
kingschan <kings.chan@qq.com>
2024-07-25 17:22:45 +0800

    LockTest

--- src/main/java/com/kingschan1204/jdk/review/juc/lock/LockTest.java
+++ src/main/java/com/kingschan1204/jdk/review/juc/lock/LockTest.java
@@ -12,24 +12,55 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class LockTest {
     private final Lock lock = new ReentrantLock();
+    volatile boolean enableTimeOut = false;
+
+    /**
+     *
+     * @param enableTimeOut 是否启用超时获取锁
+     */
+    public LockTest(boolean enableTimeOut) {
+        this.enableTimeOut = enableTimeOut;
+    }
+
+    private void execute(String threadName)   {
+       try{
+           System.out.println(threadName + " got the lock");
+           // 模拟一些工作
+           TimeUnit.SECONDS.sleep(4);
+
+       }catch (Exception e){
+           e.printStackTrace();
+       }finally {
+           System.out.println(threadName + " is releasing the lock");
+           lock.unlock(); // 确保锁释放
+       }
+    }
 
     public void performTask(String threadName) {
         System.out.println(threadName + " is trying to get the lock");
         try {
-            lock.lock(); // 线程尝试获取锁
-            System.out.println(threadName + " got the lock");
-            // 模拟一些工作
-            TimeUnit.SECONDS.sleep(4);
+            if(enableTimeOut){
+                if(lock.tryLock(1,TimeUnit.SECONDS)){
+                    execute(threadName);
+
+                }else{
+                    System.out.println(threadName +"获取锁超时,放弃！");
+                    return;
+                }
+            }else{
+                lock.lock(); // 线程尝试获取锁
+                execute(threadName);
+            }
+
+
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } finally {
-            System.out.println(threadName + " is releasing the lock");
-            lock.unlock(); // 确保锁释放
         }
     }
 
     public static void main(String[] args) {
-        LockTest competition = new LockTest();
+        // true 启用超时 超时放弃任务   false : 一直等 直到获取锁为止再执行
+        LockTest competition = new LockTest(false);
 
         Runnable task = () -> {
             String threadName = Thread.currentThread().getName();
